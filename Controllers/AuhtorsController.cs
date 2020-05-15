4eb3947dce1172cd1859eed60ddafb38a831a633
bartvanhoey <bartvanhoey@hotmail.com>
2020-05-15 20:59:24 +0200

    end-EP01

---
+++ Controllers/AuhtorsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookStoresWebAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStoresWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorsController : ControllerBase
+    {
+        private readonly BookStoresDBContext _context;
+
+        public AuthorsController(BookStoresDBContext context){
+            _context = context;
+        }
+
+        [HttpGet]
+        public IEnumerable<Author> Get()
+        {
+            return _context.Author.ToList();
+        }
+    }
+}
